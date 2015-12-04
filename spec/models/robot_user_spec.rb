78d9a583077b0fa15844338df1431f9f571d2a3d
Roy Miller <roy@theotherroad.com>
2015-12-04 10:04:15 -0500

    fix redirect_to for StartController

--- spec/models/robot_user_spec.rb
+++ spec/models/robot_user_spec.rb
@@ -20,9 +20,10 @@ describe RobotUser do
       match.game.current_player = match.game.players.first
       allow(user).to receive(:active_match).and_return(match)
       allow(match).to receive(:ask_for_cards).and_return(nil)
+      allow(match).to receive(:save!).and_return(nil)
       match.notify_observers
       expected_requestor = match.user_for_player(match.game.current_player)
       expect(match).to have_received(:ask_for_cards).once.with(hash_including(requestor: expected_requestor), any_args)
-    end
+      expect(match).to have_received(:save!).once    end
   end
 end
